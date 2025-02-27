bcaf5a38c54a9a2887a6af170f367c9e1bc198b8
4512yasir <yasiryaska1@gmail.com>
2025-02-27 18:35:14 +0300

    intro to objects

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,  
+        [key]: value  
+    };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;  // Modify the original object
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };  // Clone employee
+    delete newEmployee[key];  // Remove key
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;
+}
